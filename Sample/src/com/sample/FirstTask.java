7bc925a4e26704786b3afadc94053d86bbc264ca
PavanPatil9714 <120728861+PavanPatil9714@users.noreply.github.com>
2022-12-16 19:11:25 +0530

    First commit

---
+++ Sample/src/com/sample/FirstTask.java
@@ -0,0 +1,7 @@
+package com.sample;
+
+public class FirstTask {
+public static void main(String []args) {
+	System.out.println("hello word");
+}
+}
---
+++ Sample/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author NAMO
+ *
+ */
+module Sample {
+}
